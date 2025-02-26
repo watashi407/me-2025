34b3f37df1fc6cb20c04bb8ba48dd63a30853c41
watashi407 <hyperteddy70@gmail.com>
2025-02-26 22:37:33 +0800

    fix issue

--- components/utils/profile/OscillatingText.tsx
+++ components/utils/profile/OscillatingText.tsx
@@ -5,13 +5,13 @@ export default function OscillatingText() {
     <div className="w-fit -rotate-12 text-orange-700 homemade-apple-regular antialiased ">
       <div className="flex whitespace-nowrap animate-typing overflow-hidden border-r-5 border-r-orange-700 pr-4 py-2 tracking-tight">
         {watashiStudio.map((letter, index) => (
-          <i
+          <span
             key={index}
             className="text-[80px] max-sm:text-[40px] motion-preset-oscillate"
             style={{ animationDelay: `${index % 2 === 0 ? 1 : 2}s` }}
           >
             {letter}
-          </i>
+          </span>
         ))}
       </div>
     </div>
