49a348bcf3ceefa534fd4f57a03617ca7cd70ea1
watashi407 <hyperteddy70@gmail.com>
2025-02-27 20:55:42 +0800

    small changes

--- components/utils/profile/OscillatingText.tsx
+++ components/utils/profile/OscillatingText.tsx
@@ -2,12 +2,12 @@ export default function OscillatingText() {
   const watashiStudio = "watashi".split(""); // Example text
 
   return (
-    <div className="w-fit -rotate-12 text-orange-700 homemade-apple-regular antialiased ">
-      <div className="flex whitespace-nowrap animate-typing overflow-hidden border-r-5 border-r-orange-700 pr-4 py-2 tracking-tight">
+    <div className="homemade-apple-regular w-fit -rotate-12 text-orange-700 antialiased">
+      <div className="animate-typing border-r-5 flex overflow-hidden whitespace-nowrap border-r-orange-700 py-2 pr-4 tracking-tight">
         {watashiStudio.map((letter, index) => (
           <span
             key={index}
-            className="text-[80px] max-sm:text-[40px] motion-preset-oscillate"
+            className="motion-preset-oscillate homemade-apple-regular text-[80px] antialiased max-sm:text-[40px]"
             style={{ animationDelay: `${index % 2 === 0 ? 1 : 2}s` }}
           >
             {letter}
